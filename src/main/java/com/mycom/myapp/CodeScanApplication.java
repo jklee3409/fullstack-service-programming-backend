76a7d6487a3effdf1ebc40be9ca2ab35b0c35ca7
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-09-04 22:16:34 +0900

    first commit

---
+++ src/main/java/com/mycom/myapp/CodeScanApplication.java
@@ -0,0 +1,13 @@
+package com.mycom.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeScanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeScanApplication.class, args);
+    }
+
+}
