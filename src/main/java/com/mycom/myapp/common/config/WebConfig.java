b2ff8c9e3c6cc851e2da6b931ec085139bed7c71
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-09-05 17:00:38 +0900

    feat: WebConfig CORS 설정 추가

---
+++ src/main/java/com/mycom/myapp/common/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.mycom.myapp.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("https://fullstack-service-programming-clien.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
