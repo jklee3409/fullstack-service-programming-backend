4c853b3cd22f10c29ae22c1bee94d5c36b72af0e
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-10-16 15:30:50 +0900

    feat: GitHub OAuth 로그인 Client Layer 구현

---
+++ src/main/java/com/mycom/myapp/client/GithubClient.java
@@ -0,0 +1,52 @@
+package com.mycom.myapp.client;
+
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+@RequiredArgsConstructor
+public class GithubClient {
+    private final WebClient webClient;
+
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    private static final String GITHUB_TOKEN_PATH = "https://github.com/login/oauth/access_token";
+    private static final String GITHUB_USER_INFO_PATH = "https://api.github.com/user";
+
+    public Map<String, String> getAccessToken(String code) {
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("client_id", clientId);
+        formData.add("client_secret", clientSecret);
+        formData.add("code", code);
+
+        return webClient.post()
+                .uri(GITHUB_TOKEN_PATH)
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .body(BodyInserters.fromFormData(formData))
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<Map<String, String>>() {})
+                .block();
+    }
+
+    public Map<String, Object> getUserInfo(String accessToken) {
+        return webClient.get()
+                .uri(GITHUB_USER_INFO_PATH)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {})
+                .block();
+    }
+}
