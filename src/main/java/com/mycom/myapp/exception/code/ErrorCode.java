fe5f2023c0303bcd70402fb83f87ede75cfca317
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-10-16 15:53:29 +0900

    feat: BaseResponseDto 및 예외 처리 기본 클래스 추가

---
+++ src/main/java/com/mycom/myapp/exception/code/ErrorCode.java
@@ -0,0 +1,14 @@
+package com.mycom.myapp.exception.code;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    ;
+
+    private final int code;
+    private final String name;
+    private final String message;
+}
