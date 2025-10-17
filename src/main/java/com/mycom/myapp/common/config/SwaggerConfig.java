88a19cbdba23f9a6ebe54db6ed62f57b0ae52e04
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-10-18 00:08:10 +0900

    feat: Swagger JWT 인증을 위한 config 추가

---
+++ src/main/java/com/mycom/myapp/common/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.mycom.myapp.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        // BearerAuth (Authorization 헤더)
+        SecurityScheme bearerScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        // CookieAuth (refreshToken 쿠키)
+        SecurityScheme cookieScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.COOKIE)
+                .name("refreshToken");
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Todolist API")
+                        .description("Todolist Application API Documentation")
+                        .version("v1.0"))
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+                .addSecurityItem(new SecurityRequirement().addList("cookieAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("BearerAuth", bearerScheme)
+                        .addSecuritySchemes("cookieAuth", cookieScheme)
+                );
+    }
+}
