6550207afe9831a2758ccc10e42c616e85c4a544
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-10-17 20:46:21 +0900

    feat: Git Repository 등록 API Entity Layer 구현

---
+++ src/main/java/com/mycom/myapp/gitRepository/entity/GitRepository.java
@@ -0,0 +1,44 @@
+package com.mycom.myapp.gitRepository.entity;
+
+import com.mycom.myapp.common.entity.BaseEntity;
+import com.mycom.myapp.user.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "git_repositories")
+public class GitRepository extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, unique = true)
+    private String repoFullName;
+
+    @Column(nullable = false)
+    private String repoUrl;
+
+    @Column(nullable = false)
+    private String webhookId;
+}
