aea02b519a619b98b7ba80e2d3b9b77c68819399
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-10-19 22:01:58 +0900

    feat: 리포지토리 목록 조회 API Repository Layer 구현

--- src/main/java/com/mycom/myapp/gitRepository/repository/GitRepositoryRepository.java
+++ src/main/java/com/mycom/myapp/gitRepository/repository/GitRepositoryRepository.java
@@ -1,6 +1,8 @@
 package com.mycom.myapp.gitRepository.repository;
 
 import com.mycom.myapp.gitRepository.entity.GitRepository;
+import com.mycom.myapp.user.entity.User;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,4 +10,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface GitRepositoryRepository extends JpaRepository<GitRepository, Long> {
     Optional<GitRepository> findByRepoFullName(String repoFullName);
+    List<GitRepository> findAllByUser(User user);
 }
