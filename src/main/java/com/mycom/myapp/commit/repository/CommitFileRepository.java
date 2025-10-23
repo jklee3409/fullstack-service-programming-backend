51e3441aca9314dc1dd93f9b268d41430b5dc33c
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-10-23 19:26:49 +0900

    fix: findByCommitId 리턴 타입 리스트로 변경

--- src/main/java/com/mycom/myapp/commit/repository/CommitFileRepository.java
+++ src/main/java/com/mycom/myapp/commit/repository/CommitFileRepository.java
@@ -1,11 +1,12 @@
 package com.mycom.myapp.commit.repository;
 
 import com.mycom.myapp.commit.entity.CommitFile;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommitFileRepository extends JpaRepository<CommitFile, Long> {
-    Optional<CommitFile> findByCommitId(Long commitId);
+    List<CommitFile> findByCommitId(Long commitId);
 }
