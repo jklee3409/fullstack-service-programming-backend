63790f8e82c7dea911089f8460786b7164f5ca16
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-11-06 14:42:33 +0900

    fix: ResourceLoader를 사용하여 firebase-service-account 읽도록 수정

--- src/main/java/com/mycom/myapp/common/config/FirebaseConfig.java
+++ src/main/java/com/mycom/myapp/common/config/FirebaseConfig.java
@@ -4,6 +4,8 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import java.io.FileInputStream;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,16 +14,22 @@ import org.springframework.core.io.ClassPathResource;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
 @Configuration
+@RequiredArgsConstructor
 public class FirebaseConfig {
+
+    private final ResourceLoader resourceLoader;
 
     @Value("${firebase.service-account-path}")
     private String serviceAccountPath;
 
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        ClassPathResource resource = new ClassPathResource(serviceAccountPath);
+        Resource resource = resourceLoader.getResource(serviceAccountPath);
+
         InputStream serviceAccount = resource.getInputStream();
 
         FirebaseOptions options = FirebaseOptions.builder()
